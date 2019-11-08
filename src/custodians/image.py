9c5c3bf0d70d702f9cccf54fe2622b1ab33d87e4
Patrick Cullen <patrickbcullen@gmail.com>
2019-11-08 15:58:14 -0500

    refactored the code

--- src/custodians/image.py
+++ src/custodians/image.py
@@ -13,26 +13,31 @@ class ImageCustodian(object):
             in_use_images = self.in_use_images(region)
 
             for image in self.aws.describe_images(region):
-                tags = self.aws.tags_to_dict(image.get('Tags', {}))
+                tag_info = flatten_dict(self.aws.tags_to_dict(image.get('Tags', {})))
                 create_time = dateutil.parser.parse(image['CreationDate'])
                 days_old = age_in_days(create_time)
                 image_id = image['ImageId']
                 name = image['Name']
 
                 if days_old >= max_days:
-                    instance_id = in_use_images.get(image_id)
-                    if instance_id:
-                        self.logger.info("Skipping image in use by %s (%dd) %s %s %s" % (instance_id, days_old, image_id, name, flatten_dict(tags)))
-                        continue
-
-                    if not dryrun:
-                        self.logger.info("Deleting (%dd) %s %s %s" % (days_old, image_id, name, flatten_dict(tags)))
-                        self.aws.deregister_image(region, image_id)
-                    else:
-                        self.logger.info("Would have deleted (%dd) %s %s %s" % (days_old, image_id, name, flatten_dict(tags)))
+                    self.cleanup_unused_image(dryrun, region, in_use_images, image_id, days_old, name, tag_info)
+                else:
+                    self.logger.info("Keeping unused image because age of %sd < %sd %s %s %s" % (days_old, max_days, image_id, name, tag_info))
         except Exception as ex:
             self.logger.error("%s: %s" % (self.__class__.__name__, ex))
 
+
+    def cleanup_unused_image(self, dryrun, region, in_use_images, image_id, days_old, name, tag_info):
+        instance_id = in_use_images.get(image_id)
+        if instance_id:
+            self.logger.info("Skipping image in use by %s (%dd) %s %s %s" % (instance_id, days_old, image_id, name, tag_info))
+            return
+
+        if not dryrun:
+            self.logger.info("Deleting (%dd) %s %s %s" % (days_old, image_id, name, tag_info))
+            self.aws.deregister_image(region, image_id)
+        else:
+            self.logger.info("Would have deleted (%dd) %s %s %s" % (days_old, image_id, name, tag_info))
 
     def in_use_images(self, region):
         in_use_images = {}
